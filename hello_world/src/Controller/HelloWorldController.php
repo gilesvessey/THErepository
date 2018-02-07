58a790bb264fdc5d822f8be9730e5d666fae2422
[Daniel] <[danielebbah@yahoo.com]>
2018-02-07 09:34:39 -0400

    Daniel's Hello_World Module

---
+++ hello_world/src/Controller/HelloWorldController.php
@@ -0,0 +1,11 @@
+<?php
+    
+namespace Drupal\hello_world\Controller;
+    class HelloWorldController{
+        public function hello() {
+            return array(
+                         '#title' => 'Hello World',
+                         '#markup' => 'This is some content...'
+                         );
+        }
+    }
