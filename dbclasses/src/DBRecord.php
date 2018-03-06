8d34866e1def95c3f91d90cf44646957e504eb53
eggscendance <35234836+eggscendance@users.noreply.github.com>
2018-03-06 14:39:07 -0400

    Update DBRecord.php

--- dbclasses/src/DBRecord.php
+++ dbclasses/src/DBRecord.php
@@ -4,14 +4,15 @@ class DBRecord
 {
 	public $id;
 	public $title;
-	public $source;
 	public $issn_l;
 	public $p_issn;
 	public $e_issn;
-	public $lcclass;
 	public $callnumber;
+	public $modified;
+	public $user;
+	public $source;
 	
-	public function __construct($id, $title, $source, $issn_l, $p_issn, $e_issn, $lcclass, $callnumber)
+	public function __construct($id, $title, $source, $issn_l, $p_issn, $e_issn, $lcclass, $callnumber, $modified, $user)
 	{
 		$this->id = $id;
 		$this->title = $title;
@@ -19,8 +20,9 @@ class DBRecord
 		$this->issn_l = $issn_l;
 		$this->p_issn = $p_issn;
 		$this->e_issn = $e_issn;
-		$this->lcclass = $lcclass;
 		$this->callnumber = $callnumber;
+		$this->modified = $modified;
+		$this->user = $user;
 	}
 }
 ?>
