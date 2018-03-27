b3cedc0e6fbf3681d2290daa0323c753d2c07010
Tim <35234836+eggscendance@users.noreply.github.com>
2018-03-27 12:47:52 -0300

    Email Report

--- file_upload/src/Form/FileUploadForm.php
+++ file_upload/src/Form/FileUploadForm.php
@@ -449,11 +449,11 @@ class FileUploadForm extends FormBase {
 	readfile($fileLocation . $fileName);
 	
 	// send an email message when done
-	//$to = "tim@pro-grammering.com";
-	//$from = "no-replay@issn.researchspaces.ca";
-	//$subject = "ISSN Upload Report";
-	//$body = "Your file has been processed. Your report is available <a href='http://www.issn.researchspaces.ca/".$fileLocation . $fileName."'>HERE</a>.";
-	//simple_mail_send($from, $to, $subject, $body);
+	$to = \Drupal\user\Entity\User::load($user)->get("mail")->value;
+	$from = "no-replay@issn.researchspaces.ca";
+	$subject = "ISSN Upload Report";
+	$body = "Your file has been processed. Your report is available <a href='http://www.issn.researchspaces.ca/".$fileLocation . $fileName."'>HERE</a>.";
+	simple_mail_send($from, $to, $subject, $body);
 		
 	exit;
   }
